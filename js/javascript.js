db9169026801499bac643594096e24e7029f05e1
Linosolis23 <lino.ma41393@cesurformacion.com>
2021-12-26 23:26:39 +0100

    mitad del story 2

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,12 @@
-    function Mostrar_Carta() {
+function mostrarcarta() {
+    document.getElementById('carta1').src = "../img/carta1.png";
+    document.getElementById('carta2').src = "../img/carta2.png";
+    document.getElementById('carta3').src = "../img/carta3.png";
+    document.getElementById('carta4').src = "../img/carta4.png";
+    document.getElementById('carta5').src = "../img/carta2.png";
+    document.getElementById('carta6').src = "../img/carta3.png";
+    document.getElementById('carta7').src = "../img/carta4.png";
+    document.getElementById('carta8').src = "../img/carta1.png";
+}
 
 
-    }
