1450a434ef1ab4a9b379c4c21d862f49971b680a
Linosolis23 <lino.ma41393@cesurformacion.com>
2022-01-03 18:53:06 +0100

    Revert "cartas aleatorias puestas y arreglados los bugs"
    This reverts commit ea1572a66f0660d0936eb94508af2dd02a5ddab1.

--- js/javascript.js
+++ js/javascript.js
@@ -4,188 +4,171 @@ var contador = 0;
 var puntuacion = 0;
 //array para guardas las cartas que coinciden
 var cartasacertadas = [];
-//array para guardar las filas
-var filasCartas = [];
 // funcion para comprobar si las cartas se han destapado
-function comprobanteCarta(carta) {
-
-    var boleanoCarta = false;
+function comprobante(carta) {
+    var boleano = false;
     for (let index = 0; index < cartasacertadas.length; index++) {
+        console.log("dentro del for");
+        console.log("este es la carta 1" + carta);
         if (carta == cartasacertadas[index]) {
-            console.log("dentro del for de cartas");
-            boleanoCarta = true;
+            return boleano = true;
         }
     }
-
-    console.log(boleanoCarta);
-    return boleanoCarta;
-}
-
-
-function comprobanteFila(fila) {
-    var boleanoFila = false;
-    for (let index = 0; index < filasCartas.length; index++) {
-        if (fila == filasCartas[index]) {
-            console.log("dentro del for de filas");
-            boleanoFila = true;
-        }
-
-    }
-    console.log(boleanoFila);
-    return boleanoFila;
-
-}
-//funcion que genera numero random
-function numeroRandom(min, max) {
-    return Math.ceil(Math.random() * (max - min) + min);
+    return boleano;
 }
 
 //funcion para mostrar las cartas boca arriba
 function mostrarCarta(fila) {
 
-    //comprobamos si la fila ya esta en el array de filas llamadas
-    if (comprobanteFila(fila) == false) {
+    console.log("entrando mostrar");
 
-        console.log("entrando mostrar");
 
-        var numeroAleatorio = numeroRandom(0, 4);
-
-        var lastChar = fila.substr(fila.length - 1);
-
-        var cartaDestapada = document.getElementById('carta' + lastChar).src = "../img/carta" + numeroAleatorio + ".png";
-
-        while (comprobanteCarta(cartaDestapada) == true) {
-            console.log("dentro del while");
-            var numeroAleatorio = numeroRandom(0, 4);
-
-            var cartaDestapada = document.getElementById('carta' + lastChar).src = "../img/carta" + numeroAleatorio + ".png";
-
-        }
-
+    if (fila == "fila1") {
+        document.getElementById('carta1').src = "../img/carta1.png";
+    } else if (fila == "fila2") {
+        document.getElementById('carta2').src = "../img/carta2.png";
+    } else if (fila == "fila3") {
+        document.getElementById('carta3').src = "../img/carta3.png";
+    } else if (fila == "fila4") {
+        document.getElementById('carta4').src = "../img/carta4.png";
+    } else if (fila == "fila5") {
+        document.getElementById('carta5').src = "../img/carta2.png";
+    } else if (fila == "fila6") {
+        document.getElementById('carta6').src = "../img/carta3.png";
+    } else if (fila == "fila7") {
+        document.getElementById('carta7').src = "../img/carta4.png";
+    } else if (fila == "fila8") {
+        document.getElementById('carta8').src = "../img/carta1.png";
     }
+
+
     //llamamos a la funcion para comprobar las cartas
-    console.log("Esta es la fila que entra en comprobar fila " + fila);
-    console.log("Esta es la carta que entra en comprobar carta " + cartaDestapada);
-    comprobarCarta(fila, cartaDestapada);
+    comprobarCarta(fila);
+
+
 
 }
 
 //funcion para ocultar las cartas
 function ocultarCarta(fila) {
+    console.log("entrando en ocultar carta con la fila" + fila);
+    if (fila == "fila1") {
+        document.getElementById('carta1').src = "../img/carta_bocaabajo.jpg";
+    } else if (fila == "fila2") {
+        document.getElementById('carta2').src = "../img/carta_bocaabajo.jpg";
+    } else if (fila == "fila3") {
+        document.getElementById('carta3').src = "../img/carta_bocaabajo.jpg";
+    } else if (fila == "fila4") {
+        document.getElementById('carta4').src = "../img/carta_bocaabajo.jpg";
+    } else if (fila == "fila5") {
+        document.getElementById('carta5').src = "../img/carta_bocaabajo.jpg";
+    } else if (fila == "fila6") {
+        document.getElementById('carta6').src = "../img/carta_bocaabajo.jpg";
+    } else if (fila == "fila7") {
+        document.getElementById('carta7').src = "../img/carta_bocaabajo.jpg";
+    } else if (fila == "fila8") {
+        document.getElementById('carta8').src = "../img/carta_bocaabajo.jpg";
+    }
 
-    var lastChar = fila.substr(fila.length - 1);
-
-    document.getElementById('carta' + lastChar).src = "../img/carta_bocaabajo.jpg";
 }
 
 
-
-
-
 //variables necesarias para las cartas
-var filaCarta1 = undefined;
-var filaCarta2 = undefined;
+var carta1Id = undefined;
+var carta2Id = undefined;
 var cartaclase1 = undefined;
 var cartaclase2 = undefined;
-cartaNombre1 = undefined;
-cartaNombre2 = undefined;
 
 
 //funcion que comprueba las cartas
-function comprobarCarta(fila, cartaDestapada) {
-    console.log(fila);
+function comprobarCarta(fila) {
     contador++;
     console.log("variables inicio funcion");
+    console.log(carta1Id);
+    console.log(cartaclase1);
+    console.log(carta2Id);
+    console.log(cartaclase2);
 
-    if (filaCarta1 == undefined) {
+    if (carta1Id == undefined) {
         console.log("primer if comprobar carta");
+        var ele = document.getElementById(fila);
+
+        carta1Id = fila
+        cartaclase1 = ele.className;
 
 
-        filaCarta1 = fila
-        cartaNombre1 = cartaDestapada;
-
-    } else if (filaCarta2 == undefined) {
+    } else if (carta2Id == undefined) {
 
         console.log("segundo if comprobar carta");
+        var ele = document.getElementById(fila);
 
-
-        filaCarta2 = fila;
-        cartaNombre2 = cartaDestapada;
+        carta2Id = fila
+        cartaclase2 = ele.className;
 
     }
 
     console.log("variables despues de los if de undefined");
+    console.log(carta1Id);
+    console.log(cartaclase1);
+    console.log(carta2Id);
+    console.log(cartaclase2);
+    console.log(comprobante(carta1Id));
 
     if (contador == 2) {
 
-        if (filaCarta1 != filaCarta2 && cartaNombre1 == cartaNombre2 && (cartaNombre1 || cartaNombre2 != undefined)) {
-            console.log("LAS CARTAS SON IGUALES");
-            puntuacion++;
+        if (comprobante(carta1Id) == true && comprobante(carta2Id) == true) {
+            console.log("coinciden cartas");
 
-            cartasacertadas.push(cartaNombre1);
-            cartasacertadas.push(cartaNombre2);
-
-            filasCartas.push(filaCarta1);
-            filasCartas.push(filaCarta2);
-
-            filaCarta1 = undefined;
-            filaCarta2 = undefined;
+            carta1Id = undefined;
+            carta2Id = undefined;
             cartaclase1 = undefined;
             cartaclase2 = undefined;
-            cartaNombre1 = undefined;
-            cartaNombre2 = undefined;
-            console.log(puntuacion);
             contador = 0;
-        } else if (comprobanteFila(filaCarta1) == false && comprobanteFila(filaCarta2) == true) {
-            console.log("Ocultando la carta del primer click que no esta levantada");
-            setTimeout(ocultarCarta, 300, filaCarta1);
-            filaCarta1 = undefined;
-            filaCarta2 = undefined;
+        } else if (comprobante(carta1Id) == false && comprobante(carta2Id) == true) {
+            console.log("dentro del 2 if de cartas acertadas");
+            setTimeout(ocultarCarta, 300, carta1Id);
+            carta1Id = undefined;
+            carta2Id = undefined;
             cartaclase1 = undefined;
             cartaclase2 = undefined;
-            cartaNombre1 = undefined;
-            cartaNombre2 = undefined;
-
             contador = 0;
-        } else if (comprobanteFila(filaCarta1) == true && comprobanteFila(filaCarta2) == false) {
-            console.log("Ocultando la carta del segundo click que no esta levantada");
-            setTimeout(ocultarCarta, 300, filaCarta2);
-            filaCarta1 = undefined;
-            filaCarta2 = undefined;
+        } else if (comprobante(carta2Id) == false && comprobante(carta1Id) == true) {
+            console.log("dentro del 3 if de cartas acertadas");
+            setTimeout(ocultarCarta, 300, carta2Id);
+            carta1Id = undefined;
+            carta2Id = undefined;
             cartaclase1 = undefined;
             cartaclase2 = undefined;
-            cartaNombre1 = undefined;
-            cartaNombre2 = undefined;
-
             contador = 0;
-
-        } else if (cartaNombre1 == undefined && cartaNombre2 == undefined) {
-            console.log("LAS CARTAS ya estan levantadas");
-
-
-            filaCarta1 = undefined;
-            filaCarta2 = undefined;
-            cartaclase1 = undefined;
-            cartaclase2 = undefined;
-            cartaNombre1 = undefined;
-            cartaNombre2 = undefined;
-
-            contador = 0;
-
-        } else if (filaCarta1 != undefined && filaCarta2 != undefined) { // && comprobante(filaCarta1) == false && comprobante(filaCarta2) == false) {
-            console.log("ocultar las dos cartas");
-            setTimeout(ocultarCarta, 300, filaCarta1);
-            setTimeout(ocultarCarta, 300, filaCarta2);
-            contador = 0;
-            filaCarta1 = undefined;
-            filaCarta2 = undefined;
-            cartaclase1 = undefined;
-            cartaclase2 = undefined;
-            cartaNombre1 = undefined;
-            cartaNombre2 = undefined;
 
         }
 
+    }
+
+    if (carta1Id != carta2Id && cartaclase1 == cartaclase2 && (cartaclase1 || cartaclase2 != undefined)) {
+        console.log("LAS CARTAS SON IGUALES");
+        puntuacion++;
+        cartasacertadas.push(carta1Id);
+        cartasacertadas.push(carta2Id);
+
+        carta1Id = undefined;
+        carta2Id = undefined;
+        cartaclase1 = undefined;
+        cartaclase2 = undefined;
+        console.log(puntuacion);
+        contador = 0;
+    } else if (carta1Id != undefined && carta2Id != undefined) {
+
+        setTimeout(ocultarCarta, 300, carta1Id);
+        setTimeout(ocultarCarta, 300, carta2Id);
+        contador = 0;
+        carta1Id = undefined;
+        carta2Id = undefined;
+        cartaclase1 = undefined;
+        cartaclase2 = undefined;
 
     }
+
+
+
 }
