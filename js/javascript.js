19345ab8ee57828c07d7379b06aaaf771797fcf7
Juan <juan.villegasm21@gmail.com>
2022-01-04 12:24:33 +0100

    comprobar juego finalizado realizado, codigo comentado,añadido un total de intentos

--- js/javascript.js
+++ js/javascript.js
@@ -1,88 +1,54 @@
+//variable que cuenta el numero de intentos de juntar las parejas
+var intentos = 0;
+
 //variable para contar los numeros de clicks
 var contador = 0;
+
 // variable con la puntuacion
 var puntuacion = 0;
-//array para guardas las cartas que coinciden
-const cartasacertadas = [];
+
 //array para guardar las filas ya destapadas
 const filasCartas = [];
 
+// array con el numero de cartas para barajarlas
+//no podemos hacerlo const ya que entonces no nos dejaria desordenarlos
+var lista = [1, 2, 3, 4, 1, 2, 3, 4];
+
+//variables necesarias para las cartas
+var filaCarta1 = undefined;
+var filaCarta2 = undefined;
+var dibujoCarta1 = undefined;
+var dibujoCarta2 = undefined;
+
+//funcion que comprueba si las filas ya han sido clicadas
 function comprobanteFila(fila) {
+
     var boleanoFila = false;
+    //recorremos el array de filasCartas comprobando que las filas no han sido ya destapadas
     for (let index = 0; index < filasCartas.length; index++) {
         if (fila == filasCartas[index]) {
-
+            //si las filas ya estan destapadas cambiamos la variable boleana a true
             boleanoFila = true;
         }
-
     }
-
     return boleanoFila;
-
-}
-// funcion para comprobar si las cartas se han destapado
-function comprobante(cartaFila) {
-    var boleano = false;
-    for (let index = 0; index < cartasacertadas.length; index++) {
-        console.log("dentro del for");
-        console.log("este es la carta " + cartaFila);
-        if (cartaFila == cartasacertadas[index]) {
-            boleano = true;
-        }
-    }
-    console.log(boleano);
-    return boleano;
 }
 
-var lista = [1, 2, 3, 4, 1, 2, 3, 4];
 //funcion para barajar las cartas
 function mezclarCartas() {
-
+    /*con esta funcion ordenamos el array de modo aleatorio ya que la 2º funcion nos devuelve numeros positivos y negativos
+     colocandolos delante de cada valor desordenandolos.
+    */
     lista = lista.sort(function() { return Math.random() - 0.5 });
-
-
 }
 
 //funcion para mostrar las cartas boca arriba
 function mostrarCarta(fila) {
-
-
-    console.log("entrando mostrar");
-    var cartaDestapada = "";
-    if (fila == "fila1") {
-        console.log(lista[0]);
-        var cartaDestapada = document.getElementById('carta1').src = "../img/carta" + lista[0] + ".png";
-    } else if (fila == "fila2") {
-        console.log(lista[1]);
-        var cartaDestapada = document.getElementById('carta2').src = "../img/carta" + lista[1] + ".png";
-    } else if (fila == "fila3") {
-        console.log(lista[2]);
-        var cartaDestapada = document.getElementById('carta3').src = "../img/carta" + lista[2] + ".png";
-    } else if (fila == "fila4") {
-        console.log(lista[3]);
-        var cartaDestapada = document.getElementById('carta4').src = "../img/carta" + lista[3] + ".png";
-    } else if (fila == "fila5") {
-        console.log(lista[4]);
-        var cartaDestapada = document.getElementById('carta5').src = "../img/carta" + lista[4] + ".png";
-    } else if (fila == "fila6") {
-        console.log(lista[5]);
-        var cartaDestapada = document.getElementById('carta6').src = "../img/carta" + lista[5] + ".png";
-    } else if (fila == "fila7") {
-        console.log(lista[6]);
-        var cartaDestapada = document.getElementById('carta7').src = "../img/carta" + lista[6] + ".png";
-    } else if (fila == "fila8") {
-        console.log(lista[7]);
-        var cartaDestapada = document.getElementById('carta8').src = "../img/carta" + lista[7] + ".png";
-    }
-
-
-    //llamamos a la funcion para comprobar las cartas
-    console.log(cartaDestapada);
-    console.log(fila);
+    var cartaDestapada;
+    var lastChar = fila.substr(fila.length - 1);
+    var cartaDestapada = document.getElementById('carta' + lastChar).src = "../img/carta" + lista[lastChar - 1] + ".png";
+    //llamamos a la funcion para comprobar las cartas pasandole la fila clicada y la carta destapada
     comprobarCarta(fila, cartaDestapada);
-
-
-
 }
 
 //funcion para ocultar las cartas
@@ -91,122 +57,90 @@ function ocultarCarta(fila) {
     var lastChar = fila.substr(fila.length - 1);
     //ocultamos la carta con el ultimo valor de las filas clicadas
     document.getElementById('carta' + lastChar).src = "../img/carta_bocaabajo.jpg";
-
 }
 
+//funcion para guardar las variables de las cartas
+function guardaCartas(fila, cartaDestapada) {
+    /*si la variable filaCarta1 no esta definida entra en este if primero
+    y la define, cuando esta se encuentra definida entra en el else definiendo la 2º
+    */
+    if (filaCarta1 == undefined) {
+        //en esta variable guardamos el numero de la fila clicada
+        filaCarta1 = fila;
+        //en esta guardamos el dibujo que se encuentra en ella
+        dibujoCarta1 = cartaDestapada;
+    } else {
+        //en esta variable guardamos el numero de la fila clicada
+        filaCarta2 = fila;
+        //en esta guardamos el dibujo que se encuentra en ella
+        dibujoCarta2 = cartaDestapada;
+    }
+}
 
-//variables necesarias para las cartas
-var filaCarta1 = undefined;
-var filaCarta2 = undefined;
+//funcion que comprueba el final del juego
+function comprobarFinalJuego() {
+    var boleanoFinal = false;
+    //comprobamos que todas las cartas estan guardadas en el array y si es asi ha finalizado el juego
+    if (filasCartas.length == 8) {
+        alert("Juego Finalizado con un total de " + intentos + " intentos.");
+        alert("Gracias por participar");
+        boleanoFinal = true;
+    }
+    return boleanoFinal;
+}
 
-var cartaNombre1 = undefined;
-var cartaNombre2 = undefined;
-
-
-//funcion que comprueba las cartas
 //funcion que comprueba las cartas
 function comprobarCarta(fila, cartaDestapada) {
-    console.log(fila);
-    contador++;
-    console.log("variables inicio funcion");
-
-    if (filaCarta1 == undefined) {
-        console.log("primer if comprobar carta");
-
-
-        filaCarta1 = fila
-        cartaNombre1 = cartaDestapada;
-
-    } else if (filaCarta2 == undefined) {
-
-        console.log("segundo if comprobar carta");
-
-
-        filaCarta2 = fila;
-        cartaNombre2 = cartaDestapada;
-
-    }
-
-    console.log("variables despues de los if de undefined");
-
-    if (contador == 2) {
-
-        console.log(filaCarta1);
-        console.log(cartaNombre1);
-        console.log(filaCarta2);
-        console.log(cartaNombre2);
-        if (comprobanteFila(filaCarta1) == false && comprobanteFila(filaCarta2) == false && filaCarta1 != filaCarta2 && cartaNombre1 == cartaNombre2 && (cartaNombre1 || cartaNombre2 != undefined)) {
-            console.log("LAS CARTAS SON IGUALES");
-            puntuacion++;
-
-            cartasacertadas.push(cartaNombre1);
-            cartasacertadas.push(cartaNombre2);
-
-            filasCartas.push(filaCarta1);
-            filasCartas.push(filaCarta2);
-
+    //añadimos este if para evitar que el jugador pueda volver a seleccionar cartas
+    if (comprobarFinalJuego() == false) {
+        //sumamos un punto a contador de click
+        contador++;
+        //esta funcion recoje los valores de las cartas y los guarda en las variables locales para compararlas
+        guardaCartas(fila, cartaDestapada);
+        //si nuestro contador es igual a 2 es que hemos realizado dos clicks y entonces podemos comparar las cartas
+        if (contador == 2) {
+            intentos++;
+            /*comprobamos que la fila 1 y 2 no se encuentran ya levantadas, ademas de que el jugador no haya pulsado dos veces sobre la misma carta
+            si ambas condiciones se cumplen y ademas los dibujos de las cartas son iguales entramos en el if y sumamos puntos.
+            */
+            if (comprobanteFila(filaCarta1) == false && comprobanteFila(filaCarta2) == false && filaCarta1 != filaCarta2 && dibujoCarta1 == dibujoCarta2) {
+                //sumamos un punto.
+                puntuacion++;
+                //guardamos en el array las filas con las cartas levantadas
+                filasCartas.push(filaCarta1);
+                filasCartas.push(filaCarta2);
+                /*
+                comprobamos si la 1º carta clicada no se encuentra volteada y si la segunda si
+                en este caso el usuario a realizado un 2º click sobre una carta ya volteada y ocultamos la 1º carta
+                 */
+            } else if (comprobanteFila(filaCarta1) == false && comprobanteFila(filaCarta2) == true) {
+                //llamamos a la funcion setTimeout para ocultar la carta con delay, esto hace que nuestro javascript asincrono actue de forma sincrona
+                setTimeout(ocultarCarta, 300, filaCarta1);
+                /*
+                comprobamos si la primera carta clicada se encuentra volteada y si la segunda carta clicada no se encuentra volteada
+                en este caso giramos la carta del 2 click.
+                */
+            } else if (comprobanteFila(filaCarta1) == true && comprobanteFila(filaCarta2) == false) {
+                setTimeout(ocultarCarta, 300, filaCarta2);
+                /*
+                comprobamos que las dos cartas ya han sido volteadas
+                 */
+            } else if (comprobanteFila(filaCarta1) == true && comprobanteFila(filaCarta2) == true) {
+                alert("Las cartas pulsadas ya se encuentran volteadas");
+                //en caso que no se cumpla ninguna de las anteriores ocultamos las dos cartas
+            } else {
+                setTimeout(ocultarCarta, 300, filaCarta1);
+                setTimeout(ocultarCarta, 300, filaCarta2);
+            } //cierre if  comprobaciones
+            //comprobamos que el juego se ha finalizado justo al terminar la funcion de comparar cartas.
+            setTimeout(comprobarFinalJuego, 500);
+            //reseteamos la variables globales a undefined para que se vuelvan a sobrescribir.
             filaCarta1 = undefined;
             filaCarta2 = undefined;
-            cartaclase1 = undefined;
-            cartaclase2 = undefined;
-            cartaNombre1 = undefined;
-            cartaNombre2 = undefined;
-            console.log(puntuacion);
+            dibujoCarta1 = undefined;
+            dibujoCarta2 = undefined;
+            //reseteamos el contador de click a 0
             contador = 0;
-        } else if (comprobanteFila(filaCarta1) == false && comprobanteFila(filaCarta2) == true) {
-            console.log("Ocultando la carta del primer click que no esta levantada");
-            setTimeout(ocultarCarta, 300, filaCarta1);
-            filaCarta1 = undefined;
-            filaCarta2 = undefined;
-            cartaclase1 = undefined;
-            cartaclase2 = undefined;
-            cartaNombre1 = undefined;
-            cartaNombre2 = undefined;
-
-            contador = 0;
-        } else if (comprobanteFila(filaCarta1) == true && comprobanteFila(filaCarta2) == false) {
-            console.log("Ocultando la carta del segundo click que no esta levantada");
-            setTimeout(ocultarCarta, 300, filaCarta2);
-            filaCarta1 = undefined;
-            filaCarta2 = undefined;
-            cartaclase1 = undefined;
-            cartaclase2 = undefined;
-            cartaNombre1 = undefined;
-            cartaNombre2 = undefined;
-
-            contador = 0;
-
-        } else if (comprobanteFila(filaCarta1) == true && comprobanteFila(filaCarta2) == true) {
-            console.log("LAS CARTAS ya estan levantadas");
-
-
-            filaCarta1 = undefined;
-            filaCarta2 = undefined;
-            cartaclase1 = undefined;
-            cartaclase2 = undefined;
-            cartaNombre1 = undefined;
-            cartaNombre2 = undefined;
-
-            contador = 0;
-
-        } else if (filaCarta1 != undefined && filaCarta2 != undefined) {
-            console.log("ocultar las dos cartas");
-            setTimeout(ocultarCarta, 300, filaCarta1);
-            setTimeout(ocultarCarta, 300, filaCarta2);
-            console.log(filaCarta1);
-            console.log(filaCarta2);
-            console.log(cartaNombre1);
-            console.log(cartaNombre2);
-            contador = 0;
-            filaCarta1 = undefined;
-            filaCarta2 = undefined;
-            cartaclase1 = undefined;
-            cartaclase2 = undefined;
-            cartaNombre1 = undefined;
-            cartaNombre2 = undefined;
-
-        }
-
-
-    }
-}
+        } //cierre if contador == 2
+    } //cierre if comprobar final del juego
+} // cierre final funcion
